a9f5207546f31b2a9002137f6308eb4274d58902
zoomer <51061101+calebrwalk5@users.noreply.github.com>
2022-04-08 12:26:50 -0400

    Update camp.cpp

--- camp.cpp
+++ camp.cpp
@@ -13,9 +13,11 @@ int main() {
     int target, port;
     std::cout << "target: ";
     std::cin >> target;
+    std::cout << std::endl << "port: ";
+    std::cin >> targetport;
     if (is_port_open(target, targetport))
-        std::cout << "OPEN";
+        std::cout << port << ": open" << std::endl;
     else
-        std::cout << "CLOSED";
+        std::cout << port << ": closed" << std::endl;
     return 0;
 }
